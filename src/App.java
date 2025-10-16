3f32b6520b7041b871ae084a80ee73aa11747ab0
KatarinaVasic <amk1011817@student.hamk.fi>
2025-10-16 16:05:42 +0300

    Improving code indentation
    Improving codes readability by adding spacing and organizing blocks.

--- src/App.java
+++ src/App.java
@@ -1,47 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Printing the name of the app 
-     String name = "Printer App";
-     System.out.println(name);
 
-     //Printing the creator´s name
-     String name2 = ("This app is made by ");
-     String creator = ("Katarina");
-     System.out.println (name2 + creator);
-     //Declaring and assinging values to two numbers 
+        String name = "Printer App";
+        System.out.println(name);
 
-    double number1 =27;
-    double number2 =13;
+        //Printing the creator´s name
 
-    //Printing the values of number1 and number2
+        String name2 = ("This app is made by ");
+        String creator = ("Katarina");
+        System.out.println (name2 + creator);
+        
+        //Declaring and assinging values to two numbers 
+        
+        double number1 =27;
+        double number2 =13;
+
+        //Printing the values of number1 and number2
 
 
-    String value1 = ("The value of number 1 is "); 
-    System.out.println(value1 + number1);
+        String value1 = ("The value of number 1 is "); 
+        System.out.println(value1 + number1);
 
-    String value2 = ("The value of number 2 is ");
-    System.out.println(value2 + number2);
+        String value2 = ("The value of number 2 is ");
+        System.out.println(value2 + number2);
 
-    //Performing operations
+        //Performing operations
 
-    double multiplication = (number1 * number2);
-    String result1 = String.format("%.2f ", multiplication);
-    double sum = number1 + number2;
-    String result2 = String.format("%.2f ", sum);
-    double division = number1 / number2;
-    String result3 = String.format("%.2f ", division);
-    double subtraction = number1 - number2;
-    String result4 = String.format("%.2f ", subtraction);
+        double multiplication = (number1 * number2);
+        String result1 = String.format("%.2f ", multiplication);
+        double sum = number1 + number2;
+        String result2 = String.format("%.2f ", sum);
+        double division = number1 / number2;
+        String result3 = String.format("%.2f ", division);
+        double subtraction = number1 - number2;
+        String result4 = String.format("%.2f ", subtraction);
 
-    //Prinitng results of all operations
+        //Prinitng results of all operations
     
-    System.out.println (number1 + " * " + number2 + " = " + result1);
-    System.out.println (number1 + " + " + number2 + " = " + result2);
-    System.out.println (number1 + " / " + number2 + " = " + result3);
-    System.out.println (number1 + " - " + number2 + " = " + result4);
+        System.out.println (number1 + " * " + number2 + " = " + result1);
+        System.out.println (number1 + " + " + number2 + " = " + result2);
+        System.out.println (number1 + " / " + number2 + " = " + result3);
+        System.out.println (number1 + " - " + number2 + " = " + result4);
 
-    // End of main method
+        //End of main method
 
 
     }
-    }
+}
