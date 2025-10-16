982dd8539a218a5e131e11b3775ae55cc00cfe4f
KatarinaVasic <amk1011817@student.hamk.fi>
2025-10-16 15:59:27 +0300

    Adding comments to describe actions
    Adding multiple comments describing the following parts of the code.

--- src/App.java
+++ src/App.java
@@ -1,13 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //Printing the name of the app 
      String name = "Printer App";
      System.out.println(name);
+
+     //Printing the creator´s name
      String name2 = ("This app is made by ");
      String creator = ("Katarina");
      System.out.println (name2 + creator);
+     //Declaring and assinging values to two numbers 
 
     double number1 =27;
     double number2 =13;
+
+    //Printing the values of number1 and number2
 
 
     String value1 = ("The value of number 1 is "); 
@@ -15,6 +21,8 @@ public class App {
 
     String value2 = ("The value of number 2 is ");
     System.out.println(value2 + number2);
+
+    //Performing operations
 
     double multiplication = (number1 * number2);
     String result1 = String.format("%.2f ", multiplication);
@@ -24,13 +32,15 @@ public class App {
     String result3 = String.format("%.2f ", division);
     double subtraction = number1 - number2;
     String result4 = String.format("%.2f ", subtraction);
+
+    //Prinitng results of all operations
     
     System.out.println (number1 + " * " + number2 + " = " + result1);
     System.out.println (number1 + " + " + number2 + " = " + result2);
     System.out.println (number1 + " / " + number2 + " = " + result3);
     System.out.println (number1 + " - " + number2 + " = " + result4);
 
-    // Multiplication part, subtraction, division, sum
+    // End of main method
 
 
     }
